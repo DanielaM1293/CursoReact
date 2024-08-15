09a36819f9bb4b31e54e9d04dc2fc54cf8787531
nicolas <nicolasandresbermeo@gmail.com>
2024-08-14 19:24:36 -0500

    Primer commit al repositorio del curso de React

---
+++ Ejercicios/ejercicio1.js
@@ -0,0 +1,7 @@
+for (let tabla = 1; tabla <= 9; tabla++) {
+    console.log(`Tabla del ${tabla}:`);
+    for (let num = 1; num <= 10; num++) {
+        console.log(`${tabla} x ${num} = ${tabla * num}`);
+    }
+    console.log('');
+}
